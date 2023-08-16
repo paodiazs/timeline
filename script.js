806d6c3e4200779c88e86201a18511360450ab78
paodiazs <45045278+paodiazs@users.noreply.github.com>
2023-08-15 22:37:53 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+new fullpage('#fullpage', {  
+  // get one from 
+  // https://alvarotrigo.com/fullPage/pricing/
+  licenseKey: 'YOUR KEY HERE',
+
+  navigation: true,
+  sectionsColor: ['#D3D636', '#F8A8C5', '#FFBD59', '#89A7DB', '#C8B3EE', '#FF6260', '#FAE8E0','#FFDE59'],
+});
